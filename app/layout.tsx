3b686657d99c8e9883501d97583dbe989f1c6bd4
Abdel Nasser Manzo <abdelnassermanzo33@gmail.com>
2025-12-15 17:38:20 +0100

    Remove invalid icon file

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,6 @@ import { Toaster } from 'react-hot-toast';
 export const metadata: Metadata = {
   title: "Sneaker Commerce - Sneakers & Streetwear",
   description: "Shop the latest sneakers and streetwear fashion",
-  icons: {
-    icon: '/favicon.ico',
-  },
 };
 
 export default function RootLayout({
